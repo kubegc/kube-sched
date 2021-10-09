99b8eea6885c98e15684f524b2d7323b2e74d08b
PTTQ <luorongzhou20@otcaix.iscas.ac.cn>
2021-10-09 16:08:34 +0800

    fix

--- pkg/scheduler/algorithm/mock_single_gpu.go
+++ pkg/scheduler/algorithm/mock_single_gpu.go
@@ -22,7 +22,7 @@ func (alg *MockSingleGPU) Schedule(requestMemory, requestCore int, availableNode
 	if time.Now().Nanosecond() % 2 == 1 {
 		gpuUuid = "GPU-21f591ed-d77b-3a27-c674-51375d2e4fd9"
 	} else {
-		gpuUuid = " GPU-da33250c-6bee-6f8d-dd97-f1aa43d95783"
+		gpuUuid = "GPU-da33250c-6bee-6f8d-dd97-f1aa43d95783"
 	}
 
 	result := scheduler.Result{
